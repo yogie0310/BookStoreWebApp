fccf7e82dd156da0cf1d881b890e2019d8dc1c10
Yogender Sharma <yogender.sharma@citixsys.com>
2022-06-17 02:41:23 +0530

    Service and component added to get all books, bootstrap added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllbooksComponent } from './allbooks/allbooks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'books', component: AllbooksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
